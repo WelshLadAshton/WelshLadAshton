aa177c094ab7af6ea8d727c882a5eb9ed1b90b3b
WelshLadAshton <156332527+WelshLadAshton@users.noreply.github.com>
2024-01-11 19:43:05 +0000

    Update Minigame1.lua

--- Minigame1.lua
+++ Minigame1.lua
@@ -1,14 +1,1 @@
-print("Hello welcome to my minigame do you want to start? [y/n] \n")
-local answer1 = io.read()
-if answer1 == "y" then
-	print("Okay Lets Start!")
-	print("Who made me?")
-	local answer2 = io.read()
-	if answer2 == "Ashton" then
-		print("Well Done!")
-	else
-		print("Wrong")
-	end
-else
-	print("Okay please close my terminal.")
-end
+
